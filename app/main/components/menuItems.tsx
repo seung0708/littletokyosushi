3b8d851462b382b85753a61777e883edde006893
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-10-14 16:42:07 -0700

    got the product details page to work

--- app/main/components/menuItems.tsx
+++ app/main/components/menuItems.tsx
@@ -7,7 +7,7 @@ const MenuItems: React.FC<{products: Product[]}> = ({products}) => {
         {products.map(product => (
             <div key={product.id} className="group relative">
             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img className="h-full w-full object-cover object-center lg:h-full lg:w-full" src={product.images[0]}/>
+                <a href={`/menu/${product.id}`}><img className="h-full w-full object-cover object-center lg:h-full lg:w-full" src={product.images[0]}/></a>
             </div>
             <div className="mt-4 flex flex-col justify-between">
                 <h3>{product.title}</h3>
--- app/main/menu/[id]/page.tsx
+++ app/main/menu/[id]/page.tsx
@@ -1,12 +1,26 @@
-import {useRouter} from 'next/router';
+'use client'
+import {useParams} from 'next/navigation';
 import {products} from '@/types/products';
 
 const ProductDetailsPage: React.FC = () => {
-    const router = useRouter();
-    const {id} = router.query;
+    const {id} = useParams();
+
+    const product = products.find(product => product.id === Number(id));
+
+    if(!product) return <p>Product Not Found</p>;
 
     return(
-        <div></div>
+        <div className='max-w-7xl mx-auto grid grid-cols-2 gap-5'>
+            <div>
+                <img className='aspect-h-4 aspect-w-3' src={product.images[0]}/>
+            </div>
+            <div>
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+                <button>Order Now</button>
+            </div>
+        </div>
     )
 }
 
