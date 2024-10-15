4b7af76f81e0a81aa9668125aa48b4398bd92a5d
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-10-14 21:03:26 -0700

    building cart page

--- app/main/cart/page.tsx
+++ app/main/cart/page.tsx
@@ -1,7 +1,100 @@
 const CartPage: React.FC = () => {
     return (
-        <div className="max-w-7xl mx-auto">
-            
+        <div className="max-w-7xl mx-auto relative z-10" aria-labelledby="slide-over-title" role='dialog' aria-modal='true'>
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden='true'></div>
+            <div className="fixed inset-0 overflow-hidden">
+                <div className="absolute inset-0 overflow-hidden">
+                    <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                        <div className="pointer-events-auto w-screen max-w-md">
+                            <div className="flex h-full flex-col overflow-y-scrll bg-white shadow-xl">
+                                <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                                    <div className="flex-items-start justify-between">
+                                        <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">Shopping Cart</h2>
+                                        <div className="ml-3 flex h-7 items-center">
+                                            <button className="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
+                                                <span className="absolute -inset-0 5">
+                                                    <span className="sr-only">Close Panel</span>
+                                                    <svg className="h-6 w-6"fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" /></svg>
+                                                </span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className="mt-8">
+                                        <div className="flow-root">
+                                            <ul className="-my-6 divid-y divid-gray-200">
+                                                <li className="flex py-6">
+                                                    <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                                        <img src="https://tailwindui.com/plus/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." className="h-full w-full object-cover object-center" />
+                                                    </div>
+                                                    <div className="ml-4 flex flex-1 flex-col">
+                                                        <div>
+                                                            <div className="flex justify-between text-base font-medium text-gray-900">
+                                                                <h3><a href="#">Throwback Hip Bag</a></h3>
+                                                                <p className="ml-4">$90.00</p>
+                                                            </div>
+                                                            <p className="mt-1 text-sm text-gray-500">Salmon</p>
+                                                        </div>
+                                                        <div className="flex flex-1 items-end justify-between text-sm">
+                                                            <p className="text-gray-500">Qty 1</p>
+                                                            <div className="flex">
+                                                                <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li className="flex py-6">
+                                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                                    <img src="https://tailwindui.com/plus/img/ecommerce-images/shopping-cart-page-04-product-02.jpg" alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch." className="h-full w-full object-cover object-center" />
+                                                </div>
+
+                                                <div className="ml-4 flex flex-1 flex-col">
+                                                    <div>
+                                                    <div className="flex justify-between text-base font-medium text-gray-900">
+                                                        <h3>
+                                                        <a href="#">Medium Stuff Satchel</a>
+                                                        </h3>
+                                                        <p className="ml-4">$32.00</p>
+                                                    </div>
+                                                    <p className="mt-1 text-sm text-gray-500">Blue</p>
+                                                    </div>
+                                                    <div className="flex flex-1 items-end justify-between text-sm">
+                                                    <p className="text-gray-500">Qty 1</p>
+
+                                                    <div className="flex">
+                                                        <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                                <div className="flex justify-between text-base font-medium text-gray-900">
+                                    <p>Subtotal</p>
+                                    <p>$262.00</p>
+                                </div>
+                                <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                                <div className="mt-6">
+                                    <a href="#" className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>
+                                </div>
+                                <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                                    <p>
+                                    or
+                                    <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                        Continue Shopping
+                                        <span aria-hidden="true"> &rarr;</span>
+                                    </button>
+                                    </p>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
