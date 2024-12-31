26364fc3d88b7cc328e6c414592c552e81235e5a
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-12-30 21:12:54 -0800

    merging changes made in cart branch to customer-auth branch

--- app/context/authContext.tsx
+++ app/context/authContext.tsx
@@ -33,15 +33,15 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         const fetchUser = async () => {
             const {data: {user}, error}  = await supabase.auth.getUser();
             if (user) {
-                setUser(user);
                 console.log(user);
                 localStorage.setItem('userId', JSON.stringify(user.id));
-                
+                const storedUserId = localStorage.getItem('userId');
+                setUser(JSON.parse(storedUserId));
             } else {
                 setUser(null);
             }
             setIsLoading(false);
-            localStorage.getItem('userId');
+      
             const {data: {subscription}} = await supabase.auth.onAuthStateChange(async (_event, session) => {
                 setUser(user ?? null);
                 setIsLoading(false);
@@ -127,7 +127,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
                 throw new Error(data.error || 'Failed to sign out');
             }
             setUser(null);
-            
+            localStorage.removeItem('userId');
             redirect('/');
         } catch (error) {
             console.error('Error signing out:', error);
