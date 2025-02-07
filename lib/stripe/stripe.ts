0ff1e94acd371720b35453c3e1a3c634d3e3abf6
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-02-07 05:03:25 -0800

    updated stripe api version

--- lib/stripe/stripe.ts
+++ lib/stripe/stripe.ts
@@ -1,11 +1,9 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-01-27.acacia",
+  apiVersion: "2025-01-27.acacia", // Update to the required version
   appInfo: {
     name: "Little Tokyo Sushi",
     version: "0.1.0",
   },
 });
-
-export default stripe;
