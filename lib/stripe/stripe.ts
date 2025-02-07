f00b7f03dfa73d6492ec0da0863fed93d0fb2058
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-02-07 05:06:39 -0800

    tried updating the stripe api version again

--- lib/stripe/stripe.ts
+++ lib/stripe/stripe.ts
@@ -1,9 +1,11 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-01-27.acacia", // Update to the required version
+  apiVersion: "2025-01-27.acacia",
   appInfo: {
     name: "Little Tokyo Sushi",
     version: "0.1.0",
   },
 });
+
+export default stripe;
