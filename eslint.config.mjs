7256b3382b069501ddbc203f315ad51538d43cad
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-02-07 03:16:06 -0800

    disabled rules

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import {FlatCompat} from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+    baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+    ...compat.config({
+        extends: ['next', 'next/core-web-vitals', 'next/typescript'],
+        rules: {
+            '@next/next/no-html-link-for-pages': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@react-hooks/exhaustive-deps': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-expressions': 'off'
+        }
+    })
+]
+
+export default eslintConfig;
