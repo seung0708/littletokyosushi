a7713a1b6cb05035610f8a20c976debb1acf271c
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-02-18 09:27:19 -0800

    added suspense boundary to signin page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,37 +1,39 @@
 'use client';
 import AdminSignin from "@/components/auth/adminSignin";
+import {Suspense} from "react";
 // import CustomerSignin from "@/components/auth/customerSignin";
 //import { useState } from "react";
 
-export default function LoginPage() {
+export default function Page() {
     // const [isAdmin, setIsAdmin] = useState(false);
-
     return (
-        <div className="text-white flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-[48rem] space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold">Admin Sign in</h2>
-                </div>
-                <AdminSignin />
-            </div>
-            {/* {isAdmin && (
-                <div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="text-white flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-md w-[48rem] space-y-8">
                     <div>
-                        <h2 className="mt-6 text-center text-3xl font-extrabold">Customer Sign in</h2>
+                        <h2 className="mt-6 text-center text-3xl font-extrabold">Admin Sign in</h2>
                     </div>
-                    <CustomerSignin />
+                    <AdminSignin />
                 </div>
-            )}  */}
-            {/* // : (
-            //     <div className="max-w-md w-[48rem] space-y-8">
-            //         <div>
-            //             <h2 className="mt-6 text-center text-3xl font-extrabold">
-            //                 Sign in to your account
-            //             </h2>
-            //         </div>
-            //         <CustomerSignin/>
-            //     </div>
-            // ) */}
-        </div>
+                {/* {isAdmin && (
+                    <div>
+                        <div>
+                            <h2 className="mt-6 text-center text-3xl font-extrabold">Customer Sign in</h2>
+                        </div>
+                        <CustomerSignin />
+                    </div>
+                )}  */}
+                {/* // : (
+                //     <div className="max-w-md w-[48rem] space-y-8">
+                //         <div>
+                //             <h2 className="mt-6 text-center text-3xl font-extrabold">
+                //                 Sign in to your account
+                //             </h2>
+                //         </div>
+                //         <CustomerSignin/>
+                //     </div>
+                // ) */}
+            </div>
+        </Suspense>
     );
 }
