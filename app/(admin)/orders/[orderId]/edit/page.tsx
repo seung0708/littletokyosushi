930b5df45a2554d407b60ce02f81f04b0cd1c6d4
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-02-17 19:28:40 -0800

    added asynchronous params to order page

--- app/(admin)/orders/[orderId]/edit/page.tsx
+++ app/(admin)/orders/[orderId]/edit/page.tsx
@@ -1,29 +1,30 @@
 'use client'
 import RecentOrder from "@/components/admin/orders/recent-order";
 
-import {useState, useEffect} from 'react'
+import {useState, useEffect, use} from 'react'
 import AdminLoading from "../../../loading";
+import { Order} from '@/types/order';
 
-const OrderPage = ({ params }: { params: { orderId: string } }) => {
-    const { orderId } = params
-    const [order, setOrder] = useState()
+const OrderPage = ({ params }: { params: Promise<{ orderId: string }> }) => {
+    const { orderId } = use(params)
+    const [order, setOrder] = useState<Order>()
 
-  useEffect(() => {
-  const fetchOrder = async () => {
-    try {
-      const response = await fetch(`/api/admin/orders/${orderId}`)
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
-      const order = await response.json()
-      setOrder(order)
-    } catch (error) {
-      console.error('Error fetching order:', error)
-    }
-  }
+    useEffect(() => {
+      const fetchOrder = async () => {
+        try {
+          const response = await fetch(`/api/admin/orders/${orderId}`)
+          if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+          const order = await response.json()
+          setOrder(order)
+        } catch (error) {
+          console.error('Error fetching order:', error)
+        }
+      }
 
-  fetchOrder()
-}, [orderId])
+      fetchOrder()
+    }, [orderId])
 
-  if (!order) return <AdminLoading />
+    if (!order) return <AdminLoading />
   
     return (
         <div>
