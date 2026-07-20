9c0836f0ac44b473029b1a3b5d04df21c39d8e77
seung0708 <seung.kim0708@gmail.com>
2026-07-19 17:03:25 -0700

    updating ui

--- components/admin/header.tsx
+++ components/admin/header.tsx
@@ -3,7 +3,7 @@ import DropDownMenuComponent from "./dropdown-menu"
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-30 h-14 border-b bg-background p-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="sticky top-0 z-30 h-14 border-b p-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <div className="flex items-center justify-between md:justify-end w-full">
         <div className="md:hidden">
           <MobileNav />
