4d3bda97f3c7898f4b4737433213457a8af52725
Seung Hyun Kim <seung.kim0708@gmail.com>
2026-08-06 21:05:42 -0700

    fixing the ui with the navigation1

--- components/store/header.tsx
+++ components/store/header.tsx
@@ -32,12 +32,13 @@ export default function Header() {
 
 
     return (
-        <header id="nav" className="fixed top-0 left-0 right-0 z-50 h-16 flex items-center bg-[rgba(24,24,24,0.97)] backdrop-blur-md border-b border-white/[0.07]">
-            <Navigation1 />
-            <div className="max-w-7xl mx-auto px-6 w-full flex items-center justify-between">
+        <Navigation1 />
+        // <header id="nav" className="fixed top-0 left-0 right-0 z-50 h-16 flex items-center bg-[rgba(24,24,24,0.97)] backdrop-blur-md border-b border-white/[0.07]">
+            
+        //     <div className="max-w-7xl mx-auto px-6 w-full flex items-center justify-between">
                 
-            </div>
-        </header>
+        //     </div>
+        // </header>
     );
 }
 
--- components/store/navigation1.tsx
+++ components/store/navigation1.tsx
@@ -250,42 +250,12 @@ export default function Navigation1() {
 
       <header className="relative">
         <nav aria-label="Top">
-          {/* Top navigation */}
+          {/* Banner */}
           <div className="bg-gray-900">
-            <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-              {/* Currency selector */}
-              <form className="hidden lg:block lg:flex-1">
-                <div className="-ml-2 inline-grid grid-cols-1">
-                  <select
-                    id="desktop-currency"
-                    name="currency"
-                    aria-label="Currency"
-                    className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-gray-900 py-0.5 pr-7 pl-2 text-left text-base font-medium text-white focus:outline-2 focus:-outline-offset-1 focus:outline-white sm:text-sm/6"
-                  >
-                    {/* {currencies.map((currency) => (
-                      <option key={currency}>{currency}</option>
-                    ))} */}
-                  </select>
-                  <ChevronDownIcon
-                    aria-hidden="true"
-                    className="pointer-events-none col-start-1 row-start-1 mr-1 size-5 self-center justify-self-end fill-gray-300"
-                  />
-                </div>
-              </form>
-
+            <div className="mx-auto flex h-10 max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
               <p className="flex-1 text-center text-sm font-medium text-white lg:flex-none">
                 Get free delivery on orders over $100
               </p>
-
-              <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                <a href="#" className="text-sm font-medium text-white hover:text-gray-100">
-                  Create an account
-                </a>
-                <span aria-hidden="true" className="h-6 w-px bg-gray-600" />
-                <a href="#" className="text-sm font-medium text-white hover:text-gray-100">
-                  Sign in
-                </a>
-              </div>
             </div>
           </div>
 
