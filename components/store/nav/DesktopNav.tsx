baca78eb9b9a15f5e4a944c2a3019cf57e0277ca
Seung Hyun Kim <seung.kim0708@gmail.com>
2026-08-10 20:31:02 -0700

    modifying how the navlinks are rendered

--- components/store/nav/DesktopNav.tsx
+++ components/store/nav/DesktopNav.tsx
@@ -12,6 +12,7 @@ import Logo from './logo';
 
 export default function DesktopNav() {
     const [open, setOpen] = useState(false)
+    console.log('navigation', navigation)
     return (     
         <div className="bg-black">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -24,118 +25,39 @@ export default function DesktopNav() {
                             <PopoverGroup className="inset-x-0 bottom-0 px-4">
                                 <div className="flex h-full justify-center space-x-8">
                                     {navigation.pages.map((page, pageIdx) => (
-                                        <Popover key={page.name} className="flex">
-                                            <div className="relative flex">
-                                                <PopoverButton className="group relative flex items-center justify-center text-sm font-medium text-white transition-colors duration-200 ease-out hover:text-gray-800 data-open:text-indigo-600">
-                                                    {page.name}
-                                                    <span
-                                                        aria-hidden="true"
-                                                        className="absolute inset-x-0 -bottom-px z-30 h-0.5 transition duration-200 ease-out group-data-open:bg-indigo-600"/>
-                                                </PopoverButton>
-                                            </div>
-                                            <PopoverPanel
-                                                transition
-                                                className="absolute inset-x-0 top-full z-20 w-full bg-white text-sm text-gray-500 transition data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
-                                            >
-                                                {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                                                <div aria-hidden="true" className="absolute inset-0 top-1/2 bg-white shadow-sm" />
-                                                <div className="relative bg-white">
-                                                    <div className="mx-auto max-w-7xl px-8">
-                                                        <div className="grid grid-cols-2 items-start gap-x-8 gap-y-10 pt-10 pb-12">
-                                                            <div className="grid grid-cols-2 gap-x-8 gap-y-10">
-                                                                <div>
-                                                                    <p
-                                                                        id={`desktop-featured-heading-${pageIdx}`}
-                                                                        className="font-medium text-gray-900"
-                                                                    >
-                                                                        Featured
-                                                                    </p>
-                                                                    <ul
-                                                                        role="list"
-                                                                        aria-labelledby={`desktop-featured-heading-${pageIdx}`}
-                                                                      className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                                                    >
-                                                                        {/* {category.featured.map((item) => (
-                                                                            <li key={item.name} className="flex">
-                                                                                <a href={item.href} className="hover:text-gray-800">
-                                                                                    {item.name}
-                                                                                </a>
-                                                                            </li>
-                                                                        ))} */}
-                                                                    </ul>
-                                                                </div>
-                                                                <div>
-                                                                    <p id="desktop-categories-heading" className="font-medium text-gray-900">
-                                                                            Categories
-                                                                    </p>
-                                                                    <ul
-                                                                        role="list"
-                                                                          aria-labelledby="desktop-categories-heading"
-                                                                        className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                                                    >
-                                                                        {/* {category.categories.map((item) => (
-                                                                            <li key={item.name} className="flex">
-                                                                                <a href={item.href} className="hover:text-gray-800">
-                                                                                    {item.name}
-                                                                                </a>
-                                                                            </li>
-                                                                        ))} */}
-                                                                    </ul>
-                                                                </div>
-                                                            </div>
-                                                            <div className="grid grid-cols-2 gap-x-8 gap-y-10">
-                                                                <div>
-                                                                    <p id="desktop-collection-heading" className="font-medium text-gray-900">
-                                                                        Collection
-                                                                    </p>
-                                                                    <ul
-                                                                        role="list"
-                                                                        aria-labelledby="desktop-collection-heading"
-                                                                        className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                                                    >
-                                                                        {/* {category.collection.map((item) => (
-                                                                            <li key={item.name} className="flex">
-                                                                                <a href={item.href} className="hover:text-gray-800">
-                                                                                    {item.name}
-                                                                                </a>
-                                                                            </li>
-                                                                        ))} */}
-                                                                    </ul>
-                                                                </div>
-                                                                <div>
-                                                                    <p id="desktop-brand-heading" className="font-medium text-gray-900">
-                                                                        Brands
-                                                                    </p>
-                                                                    <ul
-                                                                        role="list"
-                                                                        aria-labelledby="desktop-brand-heading"
-                                                                        className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                                                    >
-                                                                        {/* {category.brands.map((item) => (
-                                                                            <li key={item.name} className="flex">
-                                                                                <a href={item.href} className="hover:text-gray-800">
-                                                                                    {item.name}
-                                                                                </a>
-                                                                            </li>
-                                                                        ))} */}
-                                                                     </ul>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
+                                        page.categories ? (
+                                            <Popover key={page.name} className="flex">
+                                                <div className="relative flex">
+                                                    <PopoverButton className="group relative flex items-center justify-center text-sm font-medium text-white transition-colors duration-200 ease-out hover:text-red data-open:text-indigo-600">
+                                                        {page.name}
+                                                        <span
+                                                            aria-hidden="true"
+                                                            className="absolute inset-x-0 -bottom-px z-30 h-0.5 transition duration-200 ease-out group-data-open:bg-indigo-600"
+                                                        />
+                                                    </PopoverButton>
                                                 </div>
-                                            </PopoverPanel>
-                                        </Popover>
+                                                <PopoverPanel
+                                                    transition
+                                                    className="absolute inset-x-0 top-full z-20 w-full bg-white text-sm text-gray-500 transition data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+                                                >
+                                                      <div aria-hidden="true" className="absolute inset-0 top-1/2 bg-white shadow-sm" />
+                                                        <div className="relative bg-white">
+                                                             {page.categories.map((item) => (
+                                                                        <li key={item.name} className="flex">
+                                                                        <a href={item.href} className="hover:text-gray-800">
+                                                                            {item.name}
+                                                                        </a>
+                                                                        </li>
+                                                                    ))}
+                                                        </div>
+                                                </PopoverPanel>
+                                            </Popover>
+                                        ) : (
+                                            <a key={page.name} href={page.href} className="flex items-center text-sm font-medium text-white hover: text-gray-300">
+                                                {page.name}
+                                            </a> 
+                                        )
                                     ))}
-                                {navigation.pages.map((page) => (
-                                  <a
-                                    key={page.name}
-                                    href={page.href}
-                                    className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
-                                  >
-                                    {page.name}
-                                  </a>
-                                ))}
                               </div>
                             </PopoverGroup>
                           </div>
@@ -182,3 +104,4 @@ export default function DesktopNav() {
                   </div>
     ) 
 }
+
