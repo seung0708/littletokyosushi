3a5e4ade54e047340bebde982aeee9a052d2d212
Seung Hyun Kim <seung.kim0708@gmail.com>
2026-08-15 15:48:20 -0700

    updated shoppingcart component

--- components/store/nav/ShoppingCart.tsx
+++ components/store/nav/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { useCart } from "@/app/context/cartContext";
 import {ShoppingCartIcon } from '@heroicons/react/24/outline'
 
+import Link from 'next/link'
+
 export default function ShoppingCart() {
     const { cartItems } = useCart();
 
@@ -8,14 +10,14 @@ export default function ShoppingCart() {
         <div className="flex flex-1 items-center justify-end">
             <div className="flex items-center lg:ml-8">
                 <div className="flow-root">
-                    <a href="#" className="group -m-2 flex items-center p-2">
+                    <Link href="/cart" className="group -m-2 flex items-center p-2">
                         <ShoppingCartIcon
                             aria-hidden="true"
                             className="size-6 shrink-0 text-gray-400 group-hover:text-gray-500"
                         />
-                        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{cartItems.length}</span>
                         <span className="sr-only">items in cart, view bag</span>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
